adb18a6c371762b842bfb72cb33c4a41698421eb
vorovalex <vorovalex@gmail.com>
2020-08-18 11:25:42 +0300

    new lessons

--- src/com/company/home_work/HW_1.java
+++ src/com/company/home_work/HW_1.java
@@ -14,4 +14,18 @@ package com.company.home_work;
 */
 
 public class HW_1 {
+    public static void main(String [] args){
+
+        int number1 = 10;
+        int number2 = 18;
+        int number3 = 21;
+        int number = number1;
+        number1 = number2;
+        number2 = number3;
+        number3 = number;
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+
+    }
 }
